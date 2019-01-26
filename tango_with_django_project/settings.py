dc59f7990fbff48f6fe1ac7a7e6f6f37c68b6ff5
“YUNWEN <2358706l@student.gla.ac.uk>
2019-01-26 22:45:51 +0000

    third commit

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'rango',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -73,7 +73,12 @@ TEMPLATES = [
     },
 ]
 
-
+PASSWORD_HASHERS = [
+'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+'django.contrib.auth.hashers.BCryptPasswordHasher',
+'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
 
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
@@ -101,6 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
