af3c549d0edd7e8f232617d415a71551b4dc2627
“YUNWEN <2358706l@student.gla.ac.uk>
2019-02-11 19:33:21 +0000

    Twentieth commit

--- rango/admin.py
+++ rango/admin.py
@@ -1,20 +1,13 @@
 from django.contrib import admin
 from rango.models import Category, Page
-
+from rango.models import UserProfile
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
-# Add in this class to customise the Admin Interface
-
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
 
-# Update the registration to include this customised interface
-# admin.site.register(Category)
-
-
 admin.site.register(Page, PageAdmin)
 admin.site.register(Category, CategoryAdmin)
-
-# Add in this class to customise the Admin Interface
+admin.site.register(UserProfile)
